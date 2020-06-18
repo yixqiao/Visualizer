7e3bc1551f0052861ec380b3dd33852e8868061f
Yixuan Qiao <yixqiao@gmail.com>
2020-06-18 11:16:21 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,79 @@
+window.onload = function() {
+  
+  var file = document.getElementById("audio-file");
+  var audio = document.getElementById("audio");
+  
+  file.onchange = function() {
+    var files = this.files;
+    audio.src = URL.createObjectURL(files[0]);
+    audio.load();
+    audio.play();
+    var context = new AudioContext();
+    var src = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
+
+    var canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var ctx = canvas.getContext("2d");
+
+    src.connect(analyser);
+    analyser.connect(context.destination);
+
+    analyser.smoothingTimeConstant = 0.6;
+
+    analyser.fftSize = 4096;
+
+    var bufferLength = analyser.frequencyBinCount;
+    console.log(bufferLength);
+
+    var dataArray = new Uint8Array(bufferLength);
+
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
+
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
+
+    function renderFrame() {
+      requestAnimationFrame(renderFrame);
+
+      x = 0;
+
+      analyser.getByteFrequencyData(dataArray);
+
+      var len = bufferLength / 48
+      var vol = 0
+
+      for(var i=0; i<len; i++){
+        vol += dataArray[i]
+      }
+      for(var i=len; i<bufferLength; i++){
+        dataArray.set([0], i)
+      }
+      vol /= len;
+      console.log(vol);
+
+      
+      ctx.fillStyle = `rgb(${vol},${vol},${vol})`;
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      for (var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+        
+        var r = barHeight + (25 * (i/bufferLength));
+        var g = 250 * (i/bufferLength);
+        var b = 50;
+
+        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
+        x += barWidth + 1;
+      }
+    }
+
+    audio.play();
+    renderFrame();
+  };
+};

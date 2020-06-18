ce23738237dc61127432d5bb5ce8077ea7f26bbd
Yixuan Qiao <yixqiao@gmail.com>
2020-06-18 14:35:52 -0700

    Colored background

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 window.onload = function() {
-  
+
   var file = document.getElementById("audio-file");
   var audio = document.getElementById("audio");
-  
+
   file.onchange = function() {
     var files = this.files;
     audio.src = URL.createObjectURL(files[0]);
@@ -20,7 +20,7 @@ window.onload = function() {
     src.connect(analyser);
     analyser.connect(context.destination);
 
-    analyser.smoothingTimeConstant = 0.6;
+    analyser.smoothingTimeConstant = 0.7;
 
     analyser.fftSize = 4096;
 
@@ -35,6 +35,7 @@ window.onload = function() {
     var barWidth = (WIDTH / bufferLength) * 2.5;
     var barHeight;
     var x = 0;
+    var hue = 0;
 
     function renderFrame() {
       requestAnimationFrame(renderFrame);
@@ -43,31 +44,52 @@ window.onload = function() {
 
       analyser.getByteFrequencyData(dataArray);
 
-      var len = bufferLength / 48
-      var vol = 0
+      var bl = bufferLength * 2 / 256
+      var bv = 0
+      var ml = bufferLength * 8 / 256
+      var mv = 0
+      var hl = bufferLength * 32 / 256
+      var hv = 0
 
-      for(var i=0; i<len; i++){
-        vol += dataArray[i]
-      }
-      for(var i=len; i<bufferLength; i++){
-        dataArray.set([0], i)
-      }
-      vol /= len;
-      console.log(vol);
 
-      
-      ctx.fillStyle = `rgb(${vol},${vol},${vol})`;
+      for (var i = 0; i < bl; i++) {
+        bv += dataArray[i]
+      }
+      for (var i = bl; i < ml; i++) {
+        mv += dataArray[i]
+      }
+      for (var i = ml; i < hl; i++) {
+        hv += dataArray[i]
+      }
+
+      // for(var i=0; i<ml; i++){
+      //   dataArray.set([0], i)
+      // }
+      // for(var i=hl; i<bufferLength; i++){
+      //   dataArray.set([0], i)
+      // }
+
+      bv /= bl;
+      mv /= ml;
+      hv /= hl;
+
+      hue += 0.5;
+
+      // hue, sat, val
+      ctx.fillStyle = `hsl(${(hue + hv * 90/255)%360},${mv * 100/255}%,${bv * 100/255}%)`;
       ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
       for (var i = 0; i < bufferLength; i++) {
         barHeight = dataArray[i];
-        
-        var r = barHeight + (25 * (i/bufferLength));
-        var g = 250 * (i/bufferLength);
-        var b = 50;
 
-        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+        var h = 180 * (i / bufferLength);
+        var s = barHeight * 100 / 255;
+        var v = 40 + (barHeight + (25 * (i / bufferLength))) * 20 / 255;
+
+        barHeight = dataArray[i] / 255 * HEIGHT * 2 / 3;
+
+        ctx.fillStyle = `hsl(${(hue+h) % 360 + 30},${s}%,${v}%)`;
+        ctx.fillRect(x, HEIGHT - barHeight, barWidth + 1, barHeight);
 
         x += barWidth + 1;
       }
